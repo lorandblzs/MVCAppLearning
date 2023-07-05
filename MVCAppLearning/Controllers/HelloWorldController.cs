911b9fc0f2025ca409cba3159612d94b61b2baae
lorandblzs <lorand.blzs@gmail.com>
2023-07-05 23:14:29 +0300

    Add project files.

---
+++ MVCAppLearning/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAppLearning.Models;
+
+namespace MVCAppLearning.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult  Index()  
+        {
+           DogViewModel dogo = new DogViewModel() { Name = "Sif", Age = 2 };
+            return View(dogo);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
---
+++ MVCAppLearning/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCAppLearning.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set;}
+        public int Age { get; set; }
+    }
+}
