ddb0102aab739ac05594647e457b497275138324
lorandblzs <lorand.blzs@gmail.com>
2023-07-06 18:56:39 +0300

    -

--- MVCAppLearning/Controllers/HelloWorldController.cs
+++ MVCAppLearning/Controllers/HelloWorldController.cs
@@ -5,15 +5,24 @@ namespace MVCAppLearning.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+
         public IActionResult  Index()  
         {
-           DogViewModel dogo = new DogViewModel() { Name = "Sif", Age = 2 };
-            return View(dogo);
+            return View(dogs);
         }
 
         public IActionResult Create() 
         {
-            return View();
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+           dogs.Add(dogViewModel);
+           return RedirectToAction(nameof(Index));
         }
 
         public string Hello()
